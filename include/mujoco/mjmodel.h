1aefc98e1d20bc294c275f2029a4ccce1988683e
zeagle <zeagle01@163.com>
2025-10-23 10:36:46 +0800

    feat: auto install script for python api

--- include/mujoco/mjmodel.h
+++ include/mujoco/mjmodel.h
@@ -528,7 +528,7 @@ typedef struct mjOption_ mjOption;
 //---------------------------------- mjVisual ------------------------------------------------------
 
 struct mjVisual_ {                // visualization options
-  struct {                        // global parameters
+  struct mjGlobal{                        // global parameters
     int   cameraid;               // initial camera id (-1: free)
     int   orthographic;           // is the free camera orthographic (0: no, 1: yes)
     float fovy;                   // y field-of-view of free camera (orthographic ? length : degree)
@@ -544,7 +544,7 @@ struct mjVisual_ {                // visualization options
     int   bvactive;               // visualize active bounding volumes (0: no, 1: yes)
   } global;
 
-  struct {                        // rendering quality
+  struct mjQuality{                        // rendering quality
     int   shadowsize;             // size of shadowmap texture
     int   offsamples;             // number of multisamples for offscreen rendering
     int   numslices;              // number of slices for builtin geom drawing
@@ -552,14 +552,14 @@ struct mjVisual_ {                // visualization options
     int   numquads;               // number of quads for box rendering
   } quality;
 
-  struct {                        // head light
+  struct mjHeadlight{                        // head light
     float ambient[3];             // ambient rgb (alpha=1)
     float diffuse[3];             // diffuse rgb (alpha=1)
     float specular[3];            // specular rgb (alpha=1)
     int   active;                 // is headlight active
   } headlight;
 
-  struct {                        // mapping
+  struct mjMap{                        // mapping
     float stiffness;              // mouse perturbation stiffness (space->force)
     float stiffnessrot;           // mouse perturbation stiffness (space->torque)
     float force;                  // from force units to space units
@@ -575,7 +575,7 @@ struct mjVisual_ {                // visualization options
     float actuatortendon;         // scale tendon width
   } map;
 
-  struct {                        // scale of decor elements relative to mean body size
+  struct mjScale{                        // scale of decor elements relative to mean body size
     float forcewidth;             // width of force arrow
     float contactwidth;           // contact width
     float contactheight;          // contact height
@@ -595,7 +595,7 @@ struct mjVisual_ {                // visualization options
     float frustum;                // frustum zfar plane
   } scale;
 
-  struct {                        // color of decor elements
+  struct mjRgba{                        // color of decor elements
     float fog[4];                 // fog
     float haze[4];                // haze
     float force[4];               // external force
---
+++ python/install_py_package.py
@@ -0,0 +1,121 @@
+import os
+import shutil
+import sys
+import subprocess
+from dataclasses import dataclass
+#from pydantic import BaseModel
+
+@dataclass
+class cfg_mujoco_c:
+    src_dir:str
+    build_dir:str
+    install_dir:str
+    build_type:str
+
+@dataclass
+class cfg_mujoco_py:
+    build_type:str
+
+@dataclass
+class config:
+    mujoco_c:cfg_mujoco_c
+    mujoco_py:cfg_mujoco_py
+
+def _run_cmd(cmd):
+    print(f"-- CMD : {' '.join(cmd)}")
+
+    try:
+        with subprocess.Popen(
+            cmd,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT,
+            text = True,
+            bufsize = 1,
+            encoding='utf-8',
+            universal_newlines = True
+        ) as proc:
+            for line in proc.stdout:
+                print(line, end='')
+
+            proc.wait()
+
+            if proc.returncode != 0:
+                print(f"-- CMD END failed with exit code {proc.returncode}", file=sys.stderr)
+                sys.exit(proc.returncode)
+
+    except FileNotFoundError:
+        print(f"-- CMD END Command not found: {cmd[0]}", file=sys.stderr)
+        sys.exit(127)  # 127 is standard POSIX code for "command not found"
+
+    except PermissionError:
+        print(f"-- CMD END Permission denied: {cmd[0]}", file=sys.stderr)
+        sys.exit(126)  # 126 is standard POSIX code for "not executable"
+
+    except OSError as e:
+        print(f"-- CMD END OS error while trying to run command: {e}", file=sys.stderr)
+        sys.exit(1)
+
+    print("-- CMD END succeeded!")
+
+
+def __copytree(dst, src, symlinks = False, ignore = None):
+    for item in os.listdir(src):
+        s = os.path.join(src, item)
+        d = os.path.join(dst, item)
+        if os.path.isdir(s):
+            shutil.copytree(s, d, symlinks, ignore)
+        else:
+            shutil.copy2(s, d)
+
+def _clear_dir(dir):
+    if os.path.exists(dir):
+        shutil.rmtree(dir, ignore_errors=True)
+
+def _override_copy_dir(dst,src):
+    _clear_dir(dst)
+    if not os.path.exists(dst):
+        os.mkdir(dst)
+    __copytree(dst,src)
+
+def _create_virtual_env_if_not_exists(dir):
+    if not os.path.exists(dir):
+        _run_cmd(['python', '-m', 'venv', dir])
+        f = os.path.join(dir,'Scripts','activate')
+        _run_cmd(['source', f])
+
+
+def _cmake_install_mujoco_c(cfg_mujoco_c,current_dir):
+    _clear_dir(cfg_mujoco_c.build_dir)
+
+    src_flag='-S'+ cfg_mujoco_c.src_dir
+    build_dir_flag='-B'+ cfg_mujoco_c.build_dir
+    install_flag='-DCMAKE_INSTALL_PREFIX=' + cfg_mujoco_c.install_dir
+    build_style3d_flag='-DMUJOCO_BUILD_STYLE3D=OFF'
+    version_flag='-DCMAKE_POLICY_VERSION_MINIMUM=3.5'
+    _run_cmd(['cmake', src_flag , build_dir_flag, install_flag,version_flag,build_style3d_flag])
+
+    _clear_dir(cfg_mujoco_c.install_dir)
+    _run_cmd(['cmake', '--build' ,cfg_mujoco_c.build_dir, '--target', 'install' , '--config',cfg_mujoco_c.build_type, ])
+
+    mujoco_plugin_path = os.environ['MUJOCO_PLUGIN_PATH']
+    _override_copy_dir(mujoco_plugin_path, os.path.join(cfg_mujoco_c.install_dir,'bin'))
+
+
+def _pip_install_mujoco_py(cfg_mujoco_py,current_dir):
+    _create_virtual_env_if_not_exists('.venv')
+
+    _run_cmd(['sh', './make_sdist.sh'])
+    _run_cmd(['pip', 'install', './dist/mujoco-3.3.6.tar.gz']) # make more rubost
+
+
+def install(configs):
+    current_dir = os.getcwd()
+    for cfg in configs:
+        _cmake_install_mujoco_c(cfg.mujoco_c, current_dir)
+        _pip_install_mujoco_py(cfg.mujoco_py, current_dir)
+
+configs = [ 
+    config( cfg_mujoco_c('..','../temp_build','../temp_install','Release'), cfg_mujoco_py('Release'))
+]
+
+install(configs)
